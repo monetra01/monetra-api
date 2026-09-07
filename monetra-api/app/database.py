9eb68cdb6f1f23f4098fc896d2f0b56615c6c6b7
monetra01 <bclimitada4@gmail.com>
2026-09-07 10:54:44 -0300

    Refactor database connection setup for environment variable

--- monetra-api/app/database.py
+++ monetra-api/app/database.py
@@ -1,26 +1,38 @@
+import os
 
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import declarative_base, sessionmaker
 
-DATABASE_URL = "sqlite:///./monetra.db"
+
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "sqlite:///./monetra.db"
+)
+
+connect_args = {}
+
+if DATABASE_URL.startswith("sqlite"):
+    connect_args = {"check_same_thread": False}
+
 
 engine = create_engine(
-	DATABASE_URL,
-	connect_args={"check_same_thread": False}
+    DATABASE_URL,
+    connect_args=connect_args
 )
 
 SessionLocal = sessionmaker(
-	autocommit=False,
-	autoflush=False,
-	bind=engine
+    autocommit=False,
+    autoflush=False,
+    bind=engine
 )
 
+
 Base = declarative_base()
+
+
 def get_db():
     db = SessionLocal()
     try:
         yield db
     finally:
         db.close()
-
